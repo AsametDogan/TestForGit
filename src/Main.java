91492488b96321bae2b156fee51230567d72a29d
kutlu <kutlumeydanli@gmail.com>
2021-12-17 23:52:29 +0300

    Updated by KM ;)

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ public class Main {
     public static void main(String[] args) {
         int a=5,c;
         System.out.printf("sayı : %d",a);
-        System.out.println("Samet'e ve Sekaya selam olsun");
+        System.out.println("Samet'e ve Sekaya selam olsu");
     }
 }
